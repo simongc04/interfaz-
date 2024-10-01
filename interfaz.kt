ad6ff3ed24621e07b4b26a67d9e1a9b987e332da
simon <simonescuela9@gmail.com>
2024-10-01 13:52:31 +0100

    interfaz creada

--- interfaz.kt
+++ interfaz.kt
@@ -2,12 +2,46 @@ package com.example.interfaz
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+import com.example.interfaz.ui.theme.InterfazTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            InterfazTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        message = "usuario:",
+                        from = "contraseña:",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
 
+@Composable
+fun Greeting(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(
+            text = message,
+            fontSize = 30.sp,
+            lineHeight = 146.sp,
+        )
+        Text(
+            text = from,
+            fontSize = 30.sp
+        )
     }
 }
 
